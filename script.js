44f9c11da2532a636787f7da349552175f808914
Paul Morales <paulginga@hotmail.com>
2020-08-09 15:22:37 -0500

    week4hw

--- script.js
+++ script.js
@@ -1,65 +1,78 @@
-var questions = [
+const question = document.getElementById("question");
+console.log(question);
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
     {
-        message: "Inside which HTML element do we put the JavaScript?",
-        choices: ["<scripting>", "<js>", "<javascript>", "<script>"],
-        answer: "<script>"
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1: "<scripting>",
+        choice2: "<js>",
+        choice3: "<javascript>",
+        choice4: "<script>",
+        answer: 4
     },
 
     {
-        message: "Inside which HTML element do we put the JavaScript?",
-        choices: ["Body", "head", "div", "script"],
-        answer: "Body"
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1: "Body",
+        choice2: "head",
+        choice3: "div",
+        choice4: "script",
+        answer: 1
     },
 
     {
-        message: "Which built-in method returns the length of the string?",
-        choices: ["size()", "index()", "length()", "function()"],
-        answer: "length()"
+        question: "Which built-in method returns the length of the string?",
+        choice1: "size()",
+        choice2: "index()",
+        choice3: "length()",
+        choice4: "function()",
+        answer: 3
     },
 
     {
-        message: "Which of the following function of String object returns the calling string value converted to upper case?",
-        choices: ["toLocaleUpperCase()", "toUpperCase", "toString()", "substring()"],
-        answer: "toUpperCase"
+        question: "Which of the following function of String object returns the calling string value converted to upper case?",
+        choice1: "toLocaleUpperCase()",
+        choice2: "toUpperCase",
+        choice3: "toString()",
+        choice4: "substring()",
+        answer: 2
     },
 
     {
-        message: "Which of the following function of Array object removes the last element from an array and returns that element?",
-        choices: ["pop()", "push()", "join()", "map()"],
-        answer: "pop()"
+        question: "Which of the following function of Array object removes the last element from an array and returns that element?",
+        choice1: "pop()",
+        choice2: "push()",
+        choice3: "join()",
+        choice4: "map()",
+        answer: 1
     }
 
 ];
-
-var questionCounter = 0;
-var timer = 60;
-
-
-var startButton = document.getElementById("start-btn")
-var answerButtonsEl = document.getElementById("answer-buttons");
-var cardBodyEl = document.querySelector(".card-body")
-var questionEl = document.getElementById("questions")
-
-startButton.addEventListener("click", startGame)
+console.log(questions);
+const maxQuestions = 5;
 
 function startGame() {
-    console.log("started");
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions]
 
-    startButton.style.display = "none";
-
-    cardBodyEl.style.display = "none";
-
-    answerButtonsEl.style.display = "block";
-    document.getElementById("question-container").removeAttribute("class");
-
-    showQuestion();
+    getNewQuestion();
 };
+console.log(currentQuestion)
+function getNewQuestion() {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
 
-function showQuestion(questions) {
-    questionEl.innerText = questions.message
+    question.innerText = currentQuestion.question;
+
 }
 
-
-function answerChose() {
-
-}
+startGame();
